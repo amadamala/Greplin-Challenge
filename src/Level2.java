291a36e987419bc8ad3c264d219c8ad4506c52f4
Anil <amadamala@gmail.com>
2011-04-27 19:53:23 +0530

    Renamed the files to reflect the levels in greplin challenge

---
+++ src/Level2.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+	Step 1. Use your code to compute the smallest prime fibonacci number
+	        greater than 227,000.  Call this number X.
+	Step 2. The password for level 3 is the sum of prime divisors of X + 1.
+ */
+public class FirstPrimeFibonacciLargerThanGivenNumber {
+
+	
+	public static void main(String[] args) {
+		fib(227000);
+	}
+	
+	public static void fib(long n) {
+		long f1 = 0;
+		long f2 = 1;
+		long f3 = f1 + f2;
+		System.out.print(f1 +" "+ f2 +" ");
+		while(true) {
+			System.out.print(f3 + " ");
+			f1 = f2;
+			f2 = f3;
+			f3 = f1 + f2;
+			
+			if(f3 > n && isPrime(f3)) {
+				System.out.println("First prime fibonacci larger than given n:: " + f3);
+				System.out.println(sumOfPrimeFactors((f3 + 1)));
+				break;
+			}
+		}
+	}
+	
+	public static int sumOfPrimeFactors(long n) {
+		int sum = 0;
+		Set<Long> set = new HashSet<Long>();
+		for(long i = 2; i <= Math.sqrt(n); i++) {
+			if(n % i == 0 && isPrime(i)) {
+				set.add(new Long(i));
+			}
+		}
+		System.out.println(Arrays.toString(set.toArray()));
+		for (Long p : set) {
+			sum += p;
+		}
+		return sum;
+	}
+	
+		boolean prime = true;
+		for (long i = 3; i <= Math.sqrt(n); i += 2)
+			if (n % i == 0) {
+				prime = false;
+				break;
+			}
+		if (( n%2 !=0 && prime && n > 2) || n == 2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
---
+++ src/Level3.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amadamala.timer.BucheitTimer;
+
+
+public class SubsetsOfNumbersLargestNumber {
+
+	public static final int CHAR_BASE = 65; 	// base to convert to big case character letter
+	public static int count = 0;				//subsets count
+	
+	// input list
+	public static int[] list = {3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99};
+	
+	// map the items with alphabet for ease of programming
+	public static Map<Character, Integer> m = new HashMap<Character, Integer>();
+	
+	public static void main(String[] args) {
+		String s = "";
+		for(int i = 0; i < list.length; i++) {  // -1 because last number is the biggest
+			char t = (char)(i + CHAR_BASE);	    // hack because I know there will be only 22 items
+			m.put(t, list[i]);
+			s += t;
+		}
+		
+		BucheitTimer.recordTimeWithTag("before comb1");
+		comb1(s);
+		BucheitTimer.recordTimeWithTag("after comb1");
+		BucheitTimer.finishAndReport();
+		
+		System.out.println("Number of subsets: " + count);
+	}
+
+	// TODO reduce the complexity and target to only this problem
+    // print all subsets of the characters in s
+    public static void comb1(String s) { comb1("", s); }
+
+    // print all subsets of the remaining elements, with given prefix 
+    private static void comb1(String prefix, String s) {
+        if (s.length() > 0) {
+            sumOfString(prefix + s.charAt(0));
+            comb1(prefix + s.charAt(0), s.substring(1));
+            comb1(prefix,               s.substring(1));
+        }
+    }  
+    
+    public static void sumOfString(String s) {
+
+    	// avoid 1 letter combinations
+    	if(s.trim().length() == 1) 
+    		return;
+
+    	char[] c = s.toCharArray();
+    	int sum = 0;
+
+    	for (char d : c) 
+			sum += m.get(d);
+	
+		if(Arrays.binarySearch(list, sum) >=  0 )
+			count++;
+
+    }
+}
